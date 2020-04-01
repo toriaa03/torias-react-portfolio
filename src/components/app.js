84fedd9ba15c0ab9748775647436a8192d3af82e
toriaa <moodyqv@yahoo.com>
2020-04-01 16:33:48 -0600

    second commit

---
+++ src/components/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import moment from "moment";
+
+import PortfolioContainer from "./portfolio/portfolio-container";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Victoria Faletui's Portfolio</h1>
+        <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
+        <PortfolioContainer />
+      </div>
+    )}
+}
