6f17689e15c97aa228a14e1d48587adfb20fb9c5
toriaa <moodyqv@yahoo.com>
2020-04-07 09:11:41 -0600

    made some changes to portfolio scss

---
+++ src/components/navigation/navigation-container.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
+export default class NavigationComponent extends Component {
+  constructor(props) {
+      super(props)
+
+      this.state = {
+          
+      }
+  }
+
+
+  render() {
+      return (
+        <div className="nav-wrapper">
+          <div className="left-side">
+            <div className="nav-link-wrapper">
+              <NavLink exact to="/" activeClassName="nav-link-active">
+                Home
+              </NavLink>
+            </div>
+
+            <div className="nav-link-wrapper">
+              <NavLink exact to="/about-me" activeClassName="nav-link-active">
+                About Me
+              </NavLink>
+            </div>
+
+            <div className="nav-link-wrapper">
+              <NavLink exact to="/contact" activeClassName="nav-link-active">
+                Contact
+              </NavLink>
+            </div>
+
+            <div className="nav-link-wrapper">
+              <NavLink exact to="/blog" activeClassName="nav-link-active">
+                Blog
+              </NavLink>
+            </div>                        
+          </div>
+
+          <div className="right-side">VICTORIA FALETUI</div>
+        </div>
+    );
+  }
+}
---
+++ src/components/portfolio/portfolio-item.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+export default function(props) {
+  
+        // Data that we'll need:
+        // - background image: thumb_image_url
+        // - logo
+        // - description: description
+        // - id: id
+
+        const { id, description, thumb_image_url, logo_url } = props.item;
+  return (
+      <div className="portfolio-item-wrapper">
+
+        <div
+          className="portfolio-img-background"
+          style={{
+            backgroundImage: "url(" + thumb_image_url + ")"
+          }}
+        />
+
+
+      <div className="img-text-wrapper">
+      <div className="logo-wrapper">
+        <img src={logo_url} />
+      </div>
+
+      <div className="subtitle">{description}</div>
+      </div>
+    </div>
+
+
+
+  );
+}
